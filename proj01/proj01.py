f72311af422c36fae1d13ccb70485eb9a31484c5
CollinFrance <collin.france222@gmail.com>
2017-07-10 15:45:21 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -1,10 +1,16 @@
-# Name:
-# Date:
+# Name:"Collin France"
+# Date: July 10th 2017
 
-# proj01: A Simple Program
+# project01: A Simple Program
 # This program asks the user for his/her name and age.
 # Then, it prints a sentence that says when the user will turn 100.
 
-# If you complete extensions, describe your extensions here!
-
-
+# If you complete extensions, describe your extension
+name = raw_input("Enter your name: ")
+age = raw_input("Enter your age at the start of this year: ")
+result=2017-int(age)+100
+print(name),"you will turn 100 in",(result)
+print (name),'are you allowed to see G,PG,PG-13 ,or R rated movies?'
+raw_input("Enter your favorite rating of movie: ")
+print "Nice!"
+print (name), "thank you for doing ths survey."
